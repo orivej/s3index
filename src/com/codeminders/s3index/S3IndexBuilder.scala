e7b0e1aa7dbb5a59e2ae20e2ed14ee6dfaed6d7d
Vadim Zaliva <lord@crocodile.org>
2012-05-30 14:49:27 -0700

    clean up

--- src/com/codeminders/s3index/S3IndexBuilder.scala
+++ src/com/codeminders/s3index/S3IndexBuilder.scala
@@ -1,12 +1,13 @@
 package com.codeminders.s3index
 
-import com.amazonaws.services.s3.AmazonS3
-import com.amazonaws.services.s3.AmazonS3Client
-import com.amazonaws.auth.PropertiesCredentials
 import java.io.FileInputStream
-import scala.collection.JavaConverters._
-import com.amazonaws.services.s3.model.S3ObjectSummary
+
+import scala.collection.JavaConverters.asScalaIteratorConverter
 import scala.collection.mutable.MutableList
+
+import com.amazonaws.auth.PropertiesCredentials
+import com.amazonaws.services.s3.model.S3ObjectSummary
+import com.amazonaws.services.s3.AmazonS3Client
 
 object S3IndexBuilder {
 
@@ -25,44 +26,30 @@ object S3IndexBuilder {
     val pstr = new FileInputStream("etc/AwsCredentials.properties");
     try {
       val s3 = new AmazonS3Client(new PropertiesCredentials(pstr));
-      val root = buildTree(listBucket(s3, bucket), "", 0)
+      val objects = s3.listObjects(bucket).getObjectSummaries().iterator().asScala.buffered
+      val root = buildTree(objects, "", 0)
     } finally {
       pstr.close();
     }
   }
 
-  def listBucket(s3 : AmazonS3, bucket : String) =
-    s3.listObjects(bucket).getObjectSummaries().iterator().asScala.buffered
-
-  def ident(n : Int) = {
-    print("]"); print(n); print("]")
-    for (i <- Range(0, n)) print(" ")
-  }
-
   def buildTree(i : BufferedIterator[S3ObjectSummary], name : String, level : Int) : Tree = {
 
-    ident(level); println(">"+name)
     val me = new Branch(name)
     while (i.hasNext) {
       val o = i.head
       val sname = o.getKey().split("/")
 
-      if (sname.length <= level) {
-        // end of current level
-        return me
-      }
-      if (level != 0 && sname(level-1) != name) {
+      if ((sname.length <= level) || (level != 0 && sname(level - 1) != name)) {
         // end of this branch        
         return me
       }
 
       if (sname.length == (level + 1)) {
         // new leaf
-        ident(level); println("+"+sname(level))
         me.children += new Leaf(sname(level), i.next())
       } else {
         // new branch
-        ident(level); println("*"+sname(level))
         me.children += buildTree(i, sname(level), level + 1)
       }
     }
