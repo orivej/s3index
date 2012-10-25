39939a29e5338e93502a87c002b2c7b893c85b7d
Vladimir Orlov <vorl@codeminders.com>
2012-10-25 20:12:49 +0300

    s3simpleclient subproject renamed into s3-simple-client; new project cli-tools added

--- project/Build.scala
+++ project/Build.scala
@@ -10,10 +10,20 @@ object S3IndexBuild extends Build {
     ) 
 
     lazy val root = Project(id = "s3index",
-                            base = file(".")) aggregate(s3simpleclient, web) dependsOn(s3simpleclient)
+                            base = file("."), settings = Defaults.defaultSettings  ++ Seq(helloTask)) aggregate(s3simpleclient, web, clitools) dependsOn(s3simpleclient)
 
-    lazy val s3simpleclient = Project(id = "s3simpleclient",
-                           base = file("s3simpleclient"))
+    lazy val clitools = Project(id = "clitools",
+                           base = file("cli-tools")) dependsOn(s3simpleclient)
+
+    lazy val s3simpleclient = Project(id = "s3-simple-client",
+                           base = file("s3-simple-client"))
+
+val hello = TaskKey[Unit]("hello", "Prints 'Hello World'")
+
+  val helloTask = hello := {
+	new play.core.StaticApplication(new java.io.File("web"))
+  }
+
 
     lazy val web = PlayProject("s3index-web", appVersion, appDependencies, mainLang = SCALA, path = file("web")).settings( 
     )  dependsOn(s3simpleclient)
