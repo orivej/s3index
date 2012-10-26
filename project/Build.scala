813f396d45c6a1c54e68dc40c08e3e26831c4d40
Vladimir Orlov <vorl@codeminders.com>
2012-10-26 19:21:12 +0300

    Adds Assembly plugin and 2 custom tasks

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,8 @@
 import sbt._
 import Keys._
 import PlayProject._
+import sbtassembly.Plugin._
+import AssemblyKeys._
 
 object S3IndexBuild extends Build {
 
@@ -8,23 +10,35 @@ object S3IndexBuild extends Build {
 
     val appDependencies = Seq(                                                                                                                                                 
     ) 
+    
+    val helloTask = TaskKey[Unit]("hello", "Prints 'Hello World'") 
+  
+    def hello = (streams) map { (s) => {
+    	s.log.info("Executing task Hello")
+    }}
+    
+    val byeTask = TaskKey[Unit]("bye", "Prints 'Goodbye'") 
+  
+    def bye = (streams) map { (s) => {
+    	s.log.info("Executing task Bye")
+    }}
 
     lazy val root = Project(id = "s3index",
-                            base = file("."), settings = Defaults.defaultSettings  ++ Seq(helloTask)) aggregate(s3simpleclient, web, clitools) dependsOn(s3simpleclient)
+                            base = file("."),
+                            settings = Defaults.defaultSettings ++ Seq(
+						      helloTask <<= hello dependsOn (compile in Compile),
+						      byeTask <<= bye dependsOn (helloTask)
+						    )
+                            ) aggregate(s3simpleclient, web, clitools) dependsOn(s3simpleclient)
 
     lazy val clitools = Project(id = "clitools",
-                           base = file("cli-tools")) dependsOn(s3simpleclient)
+                           base = file("cli-tools"), settings = Defaults.defaultSettings ++ assemblySettings ++ 
+                           Seq( jarName in assembly := "cli-tools-" + appVersion + ".jar")
+                           ) dependsOn(s3simpleclient)
 
     lazy val s3simpleclient = Project(id = "s3-simple-client",
                            base = file("s3-simple-client"))
-
-val hello = TaskKey[Unit]("hello", "Prints 'Hello World'")
-
-  val helloTask = hello := {
-	new play.core.StaticApplication(new java.io.File("web"))
-  }
-
-
+                           
     lazy val web = PlayProject("s3index-web", appVersion, appDependencies, mainLang = SCALA, path = file("web")).settings( 
     )  dependsOn(s3simpleclient)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,9 @@
 resolvers += Classpaths.typesafeResolver
 
+resolvers += Resolver.url("artifactory", url("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.4")
+
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.0")
 
 addSbtPlugin("play" % "sbt-plugin" % "2.0.3")
