a1852f9e315ea1116b4698f48ee560fab1a2faa5
Vladimir Orlov <vorl@codeminders.com>
2013-01-30 14:47:12 +0200

    Adds top-level menu

--- app/model/ApplicationSettings.scala
+++ app/model/ApplicationSettings.scala
@@ -7,8 +7,10 @@ class ApplicationSettings(configuration: play.api.Configuration) {
   lazy val backreferenceUrl: URL = new URL(configuration.getString("s3index.backreference.url").getOrElse("http://s3index.herokuapp.com/"))
   lazy val applicationName: String = configuration.getString("s3index.view.applicationName").getOrElse("S3Index")
   lazy val applicationDescription: String = configuration.getString("s3index.view.applicationDescription").getOrElse("Generates index.html for all files in Amazon S3 bucket.")
-  lazy val brandName: String = configuration.getString("s3index.view.brandName").getOrElse("Codeminders")
-  lazy val brandLink: String = configuration.getString("s3index.view.brandLink").getOrElse("http://codeminders.com")
+  lazy val brandName: String = configuration.getString("s3index.view.brandName").getOrElse("S3Index")
+  lazy val brandLink: String = configuration.getString("s3index.view.brandLink").getOrElse("http://code.google.com/p/s3index/")
+  lazy val aboutBrandLink: String = configuration.getString("s3index.view.aboutBrandLink").getOrElse("http://code.google.com/p/s3index/")
+  lazy val contactBrandLink: String = configuration.getString("s3index.view.contactBrandLink").getOrElse("http://code.google.com/p/s3index/")
   lazy val yearUpdated: String = configuration.getString("s3index.view.yearUpdated").getOrElse("2012")
 
 }
